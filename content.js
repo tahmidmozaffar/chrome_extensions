43420edb94a208d8e24020ec1061730b642e473a
Md Tahmid Mozaffar <tahmid1989@gmail.com>
2023-02-18 03:14:54 -0500

    first commit

---
+++ content.js
@@ -0,0 +1,33 @@
+function hide() {
+	
+	const divs = document.querySelectorAll("div");
+	console.log(divs.length);
+	
+	for (let i = 0; i < divs.length; i++) {
+		const div = divs[i];
+
+		if (div.hasAttribute("data-pagelet")) {
+			
+			const spans = div.querySelectorAll("span");
+			
+      for (let j = 0; j < spans.length; j++) {
+				const span = spans[j];				
+				if (span.textContent === "Recommended post") {					
+					div.remove();
+				}
+			}
+		}
+	}
+}
+
+hide();
+
+var timer = null;
+document.addEventListener("scroll", (event) => {
+	if (timer === null) {
+		timer = setTimeout(() => {
+			hide();
+			timer = null;
+		}, 2 * 1000);
+	}
+});
